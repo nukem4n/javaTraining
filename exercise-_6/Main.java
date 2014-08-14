42eb8789b1b3959ac0f9b21d8fa69b5b906be834
nukem4n <nukem4n@gmail.com>
2014-08-14 10:03:11 -0400

    Update Main.java
    working exercise 96 - tested and compile successfully

--- exercise-_6/Main.java
+++ exercise-_6/Main.java
@@ -6,6 +6,11 @@ public class Main {
 
   public static int sum(int[] array) {
     // write code here
-    return 0;
+    int sum = 0;
+    for(int i=0;i<array.length;i++){
+    sum+=array[i];
+    }
+    
+    return sum;
   }
 }
