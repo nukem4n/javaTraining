e14cb4156fcfe635d74dcd5fd0f568166c899deb
nukem4n <nukem4n@gmail.com>
2014-08-14 09:17:56 -0400

    Update Main.java

--- exercise-_6/Main.java
+++ exercise-_6/Main.java
@@ -1,1 +1,11 @@
-test
+public class Main {
+  public static void main(String[] args) {
+    int[] array = {5, 1, 3, 4, 2};
+    System.out.println(sum(array));
+  }
+
+  public static int sum(int[] array) {
+    // write code here
+    return 0;
+  }
+}
