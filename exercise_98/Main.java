1ece910737ca5e9882119d9665e85a6b6f0c0c90
nukem4n <nukem4n@gmail.com>
2014-08-14 14:38:17 -0400

    Create Main.java

---
+++ exercise_98/Main.java
@@ -0,0 +1,20 @@
+
+  
+  public static void main(String[] args) {
+        int[] original = {1, 2, 3, 4};
+        int[] copied = copy(original);
+
+        // change the copied
+        
+        public static int[] copy(int[] array){
+        int[] copied = new int[3];
+        
+        }
+        
+        copied[0] = 99;
+
+        // print both
+        System.out.println( "original: " + Arrays.toString(original));
+        System.out.println( "copied: " + Arrays.toString(copied));
+    }
+ 
