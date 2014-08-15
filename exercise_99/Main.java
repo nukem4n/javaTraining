afb8ae534805dd2f2904af1df6fa15aa73ef9598
nukem4n <nukem4n@gmail.com>
2014-08-15 08:59:57 -0400

    Update Main.java
    Working - compiled and tested

--- exercise_99/Main.java
+++ exercise_99/Main.java
@@ -1,1 +1,16 @@
-test
+public class Main {
+  public static void main(String[] args) {
+    int[] array = {5, 1, 3, 4, 2};
+    printArrayAsStars(array);
+  }
+
+  public static void printArrayAsStars(int[] array) {
+    for(int i=0;i<array.length;i++){
+     	
+     	 for(int j=0;j<array[i];j++){
+     	 	System.out.print("*");
+     	 }
+     	System.out.println();
+    }
+  }
+}
